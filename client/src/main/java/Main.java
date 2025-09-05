d0f19a286da48c78a3cd73da030216e92852543f
Aaron Steed <aaronhsteed@gmail.com>
2025-09-05 14:29:19 -0600

    Test1

--- client/src/main/java/Main.java
+++ client/src/main/java/Main.java
@@ -1,5 +1,5 @@
 import chess.*;
-
+// Testing
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
