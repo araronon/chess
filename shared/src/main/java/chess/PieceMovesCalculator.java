36e7f1e5b1dc9e17ef6bf2d473092efb13a0a676
Aaron Steed <aaronhsteed@gmail.com>
2025-09-15 11:12:25 -0600

    Working on BishopsMoveCalculator - fixing syntax issues and working to make the first text work.

--- shared/src/main/java/chess/PieceMovesCalculator.java
+++ shared/src/main/java/chess/PieceMovesCalculator.java
@@ -1,19 +1,18 @@
 package chess;
 
+import java.util.ArrayList;
 import java.util.Collection;
-import java.util.List;
-import java.util.Objects;
 
 public class PieceMovesCalculator {
 
-    ChessBoard board;
-    ChessPosition myPosition;
-    Collection<ChessMove> availableMoves;
+    private ChessBoard board;
+    private ChessPosition myPosition;
+    private Collection<ChessMove> availableMoves;
 
     public PieceMovesCalculator(ChessBoard board, ChessPosition myPosition) {
         this.board = board;
         this.myPosition = myPosition;
-        this.availableMoves = availableMoves;
+        this.availableMoves = new ArrayList<>();
     }
 
     Collection<ChessMove> MoveCalculation() {
@@ -23,21 +22,22 @@ public class PieceMovesCalculator {
                 availableMoves = new BishopMovesCalculator(board, myPosition).getMoves();
                 break;
 
-
         }
-        if (piece.getPieceType() == ChessPiece.PieceType.BISHOP)
+        return availableMoves;
     }
-
-
 
 }
 
 class BishopMovesCalculator extends PieceMovesCalculator {
 
     private Collection<ChessMove> moves;
+    private ChessBoard board;
+    private ChessPosition myPosition;
 
     public BishopMovesCalculator(ChessBoard board, ChessPosition myPosition) {
-        super(board, myPosition);
+        super();
+        this.board = board;
+        this.myPosition = myPosition;
     }
 
     public Collection<ChessMove> getMoves() {
@@ -46,78 +46,25 @@ class BishopMovesCalculator extends PieceMovesCalculator {
         ChessPiece currentPiece = board.getPiece(myPosition);
         // Iterate from the piece going from the inside out in all directions available (4 for bishop), stopping when hitting a piece or an edge. If you find a piece that is the enemy's then include it as a possible move (for capture)
         // Append all these possible moves to the Collection of Chess Moves and return the Collection.
-
-        // diag up - for-loop
-        int[][] directions = {{1,1},{1,-1},{-1}}
-        for (int i = 1; i < 10; i++) {
-            int checkCol = currentCol + i;
-            int checkRow = currentRow + i;
-            ChessPosition checkPos = new ChessPosition(checkRow, checkCol);
-            ChessPiece checkPiece = board.getPiece(checkPos);
-            if (checkRow <= 0 | checkCol <= 0 | checkRow >= 9 | checkCol >= 9 | checkPiece.getTeamColor() == currentPiece.getTeamColor()){
-                break;
-        } else {
-                moves.add(new ChessMove(myPosition,checkPos,null));
-            }
-        }
-
-        for (int i = 1; i < 10; i++) {
-            int checkCol = currentCol + -i;
-            int checkRow = currentRow + i;
-            ChessPosition checkPos = new ChessPosition(checkRow, checkCol);
-            ChessPiece checkPiece = board.getPiece(checkPos);
-            if (checkRow <= 0 | checkCol <= 0 | checkRow >= 9 | checkCol >= 9 | checkPiece.getTeamColor() == currentPiece.getTeamColor()){
-                break;
-            } else {
-                moves.add(new ChessMove(myPosition,checkPos,null));
-            }
-        }
-
-        for (int i = 1; i < 10; i++) {
-            int checkCol = currentCol + i;
-            int checkRow = currentRow + -i;
-            ChessPosition checkPos = new ChessPosition(checkRow, checkCol);
-            ChessPiece checkPiece = board.getPiece(checkPos);
-            if (checkRow <= 0 | checkCol <= 0 | checkRow >= 9 | checkCol >= 9 | checkPiece.getTeamColor() == currentPiece.getTeamColor()){
-                break;
-            } else {
-                moves.add(new ChessMove(myPosition,checkPos,null));
-            }
-        }
-
-        for (int i = 1; i < 10; i++) {
-            int checkCol = currentCol + -i;
-            int checkRow = currentRow + -i;
-            ChessPosition checkPos = new ChessPosition(checkRow, checkCol);
-            ChessPiece checkPiece = board.getPiece(checkPos);
-            if (checkRow <= 0 | checkCol <= 0 | checkRow >= 9 | checkCol >= 9 | checkPiece.getTeamColor() == currentPiece.getTeamColor()){
-                break;
-            } else {
-                moves.add(new ChessMove(myPosition,checkPos,null));
+        int[][] directions = {{1, 1}, {1, -1}, {-1, 1}, {-1, -1}};
+        for (int dirVec = 0; dirVec < directions.length; dirVec++) {
+            int dirRow = directions[dirVec][0];
+            int dirCol = directions[dirVec][1];
+            for (int i = 1; i < 10; i++) {
+                int checkCol = currentCol + dirCol * i;
+                int checkRow = currentRow + dirRow * i;
+                ChessPosition checkPos = new ChessPosition(checkRow, checkCol);
+                ChessPiece checkPiece = board.getPiece(checkPos);
+                if (checkRow <= 0 || checkCol <= 0 || checkRow >= 9 || checkCol >= 9 || checkPiece.getTeamColor() == currentPiece.getTeamColor()) {
+                    break;
+                } else {
+                    moves.add(new ChessMove(myPosition, checkPos, null));
+                }
             }
         }
         return moves;
-    // Recursion base-case
+    }
 
-//    public ChessMove bishopMove(ChessMove move) {
-//        ChessPosition endPosition = move.getEndPosition();
-//        ChessPosition newEndPosition = new ChessPosition(endPosition.getColumn() + 1, endPosition.getRow() + 1);
-//        ChessMove newMove = new ChessMove(move.getStartPosition(),newEndPosition, null);
-//        return bishopMove;
-//    }
-//
-//    public Collection<ChessMove> recursiveMove(ChessPosition position, List<ChessMove> moveList) {
-//        ChessPiece piece = board.getPiece(myPosition);
-//        if (piece != null) {
-//            if (piece.ChessGame.TeamColor.getTeamColor() == WHITE ) {
-//                return;
-//            }
-//            else {
-//                moveCollection.add(move);
-//            }
-//        }
-//        return recursiveMove(function, move);
-//    }
 
 
 }
