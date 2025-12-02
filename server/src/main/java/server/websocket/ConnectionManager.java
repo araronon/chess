726b39f3286f41d8f786ce1163339bc32b82efbd
Aaron Steed <aaronhsteed@gmail.com>
2025-12-02 16:30:41 -0700

    Began adding the needed class for Phase6 of the chess project. WebSocket handler.

---
+++ server/src/main/java/server/websocket/ConnectionManager.java
@@ -0,0 +1,31 @@
+package server.websocket;
+
+import org.eclipse.jetty.websocket.api.Session;
+import webSocketMessages.Notification;
+
+import java.io.IOException;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConnectionManager {
+    // store a map of sessions (key is gameID)
+    public final ConcurrentHashMap<Session, Session> connections = new ConcurrentHashMap<>();
+
+    public void add(Session session) {
+        connections.put(session, session);
+    }
+
+    public void remove(Session session) {
+        connections.remove(session);
+    }
+
+    public void broadcast(Session excludeSession, Notification notification) throws IOException {
+        String msg = notification.toString();
+        for (Session c : connections.values()) {
+            if (c.isOpen()) {
+                if (!c.equals(excludeSession)) {
+                    c.getRemote().sendString(msg);
+                }
+            }
+        }
+    }
+}
